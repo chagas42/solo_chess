b5aeb7c771d851dde1a55ef77a90e807e9573efb
Celso Chagas <celso.chagas.dev@gmail.com>
2026-05-27 01:02:13 -0300

    add board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Hello, world!");
 }
+
+enum Piece {
+    King,
+    Queen,
+    Tower,
+    Bishop,
+    Knight,
+    Pawn,
+}
+
+pub struct Board([Option<Piece>; 64]);
+
+impl Board {
+    fn render(&self) {
+        for (i, square) in self.0.iter().enumerate() {
+            if i % 8 == 0 && i != 0 {
+                println!();
+            } else {
+                println!(". test");
+            }
+        }
+    }
+}
